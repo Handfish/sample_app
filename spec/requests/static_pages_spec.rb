53cd326f51ecd948686263ac6f5d938e308e99d1
Kenny Udovic <kennyudovic@gmail.com>
2016-01-09 21:10:01 -0500

    Finished static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,13 +6,31 @@ RSpec.describe "StaticPages", type: :request do
       get '/static_pages/home'
       expect(response).to have_http_status(200)
     end
+
+    it "should have the right title" do
+       visit '/static_pages/home'
+       expect(page).to have_title "Home"
+     end
+
+     it "should have the h1 'Sample App'" do
+       visit '/static_pages/home'
+       expect((page).find('h1')).to have_content 'Sample App'
+     end
+
   end
 
   describe "Help Page" do
-     it "should jave the content 'Help' - is located in h1 tag" do
+     it "should jave the content 'Help'" do
        visit '/static_pages/help'
        expect(page).to have_content('Help')
      end
   end
 
+  describe "About page" do
+     it "should have the content 'About Us'" do
+       visit '/static_pages/about'
+       expect(page).to have_content('About Us')
+     end
+  end
+  
 end
